fd7054f14b8664322958e0224b33f2501f100c1b
Gregory Krulin <30390522+greg-krulin@users.noreply.github.com>
2023-05-10 22:18:34 -0700

    Add files via upload
    Finished main function bank_run_sim. Needs more testing to find any bugs. Need to add functions to display plots for user

--- bank_run.py
+++ bank_run.py
@@ -17,10 +17,10 @@ class BankRun:
         """
 
         self.total_capital = total_capital
-        self.rwa = rwa
+        #self.rwa = rwa
         self.initial_cash = initial_cash_on_hand
         self.num_iterations = num_iterations
-        self.credit_risk_assets = rwa['credit_risk']
+        self.credit_risk_assets = rwa#['credit_risk']
 
 
     def bank_run_sim(self):
@@ -43,49 +43,99 @@ class BankRun:
 
         probabilities = np.array([0.2, 0.3, 0.3, 0.1, 0.1])
 
-        remaining_cash_list = []
+        discount_rates = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6])
+
+        sim_index = []
+
+        withdrawel_list = []
+
+        capital_remaining = []
+
+        risk_assets_remaining = []
+
+        discount_rate_list = []
 
         cet1_list = []
+        
+        success = []
 
-        discount_rates = []
+        
         
         
 
         #succes_fail_list = []
 
-        for _ in range(self.num_iterations):
-            risk_assests = self.credit_risk_assets
+        for n in range(self.num_iterations):
+            sim_index.append(n)
+
+            credit_risk_assets = self.credit_risk_assets
             capital = self.total_capital
+
+            if (n%10):
+                print(f"Attempt number: {n}" )
 
             # Sample deposit withdrawals based on probabilities
             deposit_withdrawals = np.random.choice(p_withdrawal, p=probabilities) * self.initial_cash
 
+            withdrawel_list.append(deposit_withdrawals)
+
             # Calculate remaining cash
-            remaining_cash = self.initial_cash - deposit_withdrawals
+            #remaining_cash = self.initial_cash - deposit_withdrawals
 
             #remaining_cash_list = remaining_cash_list.append(remaining_cash)
 
             if deposit_withdrawals> self.total_capital:
-                discount_rate = np.random.choice(discount_rates)
-                assests_to_sell = (deposit_withdrawals - self.total_capital)/(1-discount_rate)
-            
-                if assests_to_sell > risk_assests:
-                    #bank failure neeed to implement
-                    pass
 
-                risk_assests -= assests_to_sell
+                #cash_needed is how much cash is needed to cover withrdrawels
+                cash_needed = (deposit_withdrawals - self.total_capital)
+
+                discount_rate = np.random.choice(discount_rates)
+                discount_rate_list.append(discount_rate)
+
+                #since bank will be selling at discount, will need to sell loans whose face value is more than what it will sell for. Since it is being sold at discount_rate, will need to find amount that will equal to cash_needed.
+                assests_to_sell = cash_needed/(1-discount_rate)
+            
+                if assests_to_sell > credit_risk_assets:
+                    #bank failure neeed to implement
+                    capital_remaining.append(0)
+
+                    risk_assets_remaining.append(credit_risk_assets - assests_to_sell)
+
+                    cet1_list.append(0)
+                    success.append(False)
+
+                    break
+                    
+
+                #The bank sells of loans, reducing it's total risk_assests
+                credit_risk_assets  -= assests_to_sell
+                # The bank incurs a loss equal to the discount on the sold loans, which reduces its capital
+                capital -= assests_to_sell * discount_rate
+
             
             else:
+                #Bank has enough cash at hand to cover withdrawal and stay within proper capital ratios, however this will cause a hit to their capital.
                 capital -= deposit_withdrawals
+                discount_rate_list.append(0)
 
             # Check if bank meets minimum cash reserve requirements
-            cet1 = utility.cet1(capital, risk_assests)
 
+            capital_remaining.append(capital)
+            risk_assets_remaining.append(credit_risk_assets)
 
+            cet1 = utility.cet1(capital, credit_risk_assets)
+
+            
            
-            cet1_list = cet1_list.append(cet1)
+            cet1_list.append(cet1)
 
-            if (cet1*100) >= 8.00:
+            success.append((cet1*100 >= 8.00))
                 
 
-        return {"survival_count" :successful_iterations, "faliure_count": (self.num_iterations-successful_iterations), "remaining_cash_list": remaining_cash_list, "cet1_list": cet1_list}
+        data = [sim_index, withdrawel_list, capital_remaining, risk_assets_remaining, discount_rate_list, cet1_list, success]
+        results = pd.DataFrame(data)#, columns=['simulation_number', 'withdrawal_amt', 'capital', 'credit_risk_assets', 'discount_rate',  'cet1_ratio', 'success'])  
+        results = results.transpose()
+        results.columns = ['simulation_number', 'withdrawal_amt', 'capital', 'credit_risk_assets', 'discount_rate',  'cet1_ratio', 'success']
+        results = results.set_index('simulation_number')
+
+        return results
